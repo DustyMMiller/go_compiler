b9f095c096e96e29a277b59461911c155971710d
Dusty Miller <dusty.miller109@gmail.com>
2023-11-07 19:10:50 -0500

    Finish chapter 4 and pass all tests

--- vm/vm.go
+++ vm/vm.go
@@ -92,6 +92,11 @@ func (vm *VM) Run() error {
 			if !isTruthy(condition) {
 				ip = pos - 1
 			}
+		case code.OpNull:
+			err := vm.push(Null)
+			if err != nil {
+				return err
+			}
 		}
 	}
 	return nil
@@ -202,6 +207,8 @@ func (vm *VM) executeBangOperator() error {
 		return vm.push(False)
 	case False:
 		return vm.push(True)
+	case Null:
+		return vm.push(True)
 	default:
 		return vm.push(False)
 	}
@@ -221,6 +228,8 @@ func isTruthy(obj object.Object) bool {
 	switch obj := obj.(type) {
 	case *object.Boolean:
 		return obj.Value
+	case *object.Null:
+		return false
 	default:
 		return true
 	}
