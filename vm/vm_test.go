0a52cfaf9497e1f919b55b4e1e020bbce84d6f50
Dusty Miller <dusty.miller109@gmail.com>
2023-11-18 12:59:37 -0500

    Add function name to parser

--- vm/vm_test.go
+++ vm/vm_test.go
@@ -521,6 +521,57 @@ func TestClosures(t *testing.T) {
 	runVmTests(t, tests)
 }
 
+func TestRecursiveFunctions(t *testing.T) {
+	tests := []vmTestCase{
+		{
+			input: `
+			let countDown = fn(x) {
+				if (x==0) {
+					return 0;
+				} else {
+					countDown(x-1);
+				}
+			};
+			countDown(1);
+			`,
+			expected: 0,
+		},
+		{
+			input: `
+			let countDown = fn(x) {
+				if (x == 0) {
+					return 0
+				} else {
+					countDown(x-1);
+				}
+			};
+			let wrapper = fn() {
+				countDown(2);
+			};
+			wrapper();
+			`,
+			expected: 0,
+		},
+		{
+			input: `
+			let wrapper = fn() {
+				let countDown = fn(x) {
+					if (x == 0) {
+						return 0
+					} else {
+						countDown(x-1);
+					}
+				};
+				countDown(2);
+			};
+			wrapper();
+			`,
+			expected: 0,
+		},
+	}
+	runVmTests(t, tests)
+}
+
 func runVmTests(t *testing.T, tests []vmTestCase) {
 	t.Helper()
 
@@ -530,6 +581,16 @@ func runVmTests(t *testing.T, tests []vmTestCase) {
 		err := comp.Compile(program)
 		if err != nil {
 			t.Fatalf("compiler error: %s", err)
+		}
+		for i, constant := range comp.Bytecode().Constants {
+			fmt.Printf("CONSTANT %d %p (%T):\n", i, constant, constant)
+			switch constant := constant.(type) {
+			case *object.CompiledFunction:
+				fmt.Printf("Instructions:\n%s", constant.Instructions)
+			case *object.Integer:
+				fmt.Printf("Value: %d\n", constant.Value)
+			}
+			fmt.Printf("\n")
 		}
 		vm := New(comp.Bytecode())
 		err = vm.Run()
